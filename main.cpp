673f27bd1b224da388f224f39c95cf051112447f
Alex <AlexDankoXXL@gmail.com>
2021-09-16 18:53:57 +0300

    Start

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleTitleA("NoteManager");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	
+    std::cout << "Планировщик заметок!\n";
+
+	return 0;
+}
+
