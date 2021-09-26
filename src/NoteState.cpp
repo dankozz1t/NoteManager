677ce4c5f2d8676fba3e30eb98646c2047b4b6ab
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-09-26 15:03:49 +0300

    +refactoring

---
+++ src/NoteState.cpp
@@ -0,0 +1,71 @@
+﻿#include "NoteState.h"
+#include <iostream>
+#include "LOGS.h"
+#include "Note.h"
+
+//---------------DeferredNote------------
+
+std::string DeferredNote::currentState(){ return "ОТЛОЖЕННАЯ"; }
+
+void DeferredNote::previous(Note* note)
+{
+#ifdef LOGS
+	std::cout << " Задача и так в отложеных " << std::endl;
+#endif
+}
+
+void DeferredNote::next(Note* note)
+{
+#ifdef LOGS
+	std::cout << " Перемещенно в активные " << std::endl;
+#endif
+
+	note->setState(new ActiveNote);
+	note->enterDate();
+}
+
+
+//---------------ActiveNote------------
+
+std::string ActiveNote::currentState() { return "АКТИВНАЯ"; }
+
+void ActiveNote::previous(Note* note)
+{
+#ifdef LOGS
+	std::cout << " Перемещенно в отложенные " << std::endl;
+#endif
+	note->setState(new DeferredNote);
+	note->setimportant(false);
+	note->enterDate();
+}
+
+void ActiveNote::next(Note* note)
+{
+#ifdef LOGS
+	std::cout << " Задача завершена (Галочка поставлена) " << std::endl;
+#endif
+	note->setState(new CompletedNote);
+	note->setimportant(false);
+	note->enterDate();
+}
+
+
+//---------------CompletedNote------------
+
+std::string CompletedNote::currentState() { return "ЗАВЕРШЕННОЕ"; }
+
+void CompletedNote::previous(Note* note)
+{
+#ifdef LOGS
+	std::cout << " Галочка снята " << std::endl;
+#endif
+	note->setState(new ActiveNote);
+	note->enterDate();
+}
+
+void CompletedNote::next(Note* note)
+{
+#ifdef LOGS
+	std::cout << " задача и так завершеная" << std::endl;
+#endif
+}
---
+++ src/NoteState.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <string>
+
+class Note;
+
+class INoteState
+{
+public:
+	virtual std::string currentState() = 0;
+
+	virtual void next(Note* note) = 0;
+	virtual void previous(Note* note) = 0;
+};
+
+
+class DeferredNote :public INoteState
+{
+public:
+	std::string currentState() override;
+
+	void next(Note* note) override;
+	void previous(Note* note) override;
+};
+
+
+class ActiveNote : public INoteState
+{
+public:
+	std::string currentState() override;
+
+	void next(Note* note) override;
+	void previous(Note* note) override;
+};
+
+
+class CompletedNote :public INoteState
+{
+public:
+	std::string currentState() override;
+
+	void next(Note* note) override;
+	void previous(Note* note) override;
+};
+
